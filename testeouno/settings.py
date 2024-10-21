aa6577481e906c9784c46a1e05e65fbc0681384d
idk2345uwu <163457466+idk2345uwu@users.noreply.github.com>
2024-10-21 01:59:25 -0300

    aaa

--- testeouno/settings.py
+++ testeouno/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'testeouno.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DJANGO_DEMO_EMP',
+        'USER': 'root',
+        'PASSWROD': '',
+
     }
 }
 
