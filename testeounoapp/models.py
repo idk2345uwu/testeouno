7d5daf2b29e202784ac77c37f789fd0c151c98ef
idk2345uwu <163457466+idk2345uwu@users.noreply.github.com>
2024-10-27 18:28:31 -0300

    Grupo

--- testeounoapp/models.py
+++ testeounoapp/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Proyecto(models.Model):
-    fechaInicio = models.DateField()
-    fechaTermino = models.DateField()
-    nombre = models.CharField(max_length=50)
-    responsable = models.CharField(max_length=50)
-    prioridad = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    rut = models.IntegerField()
+    edad = models.IntegerField()
+    grupo = models.CharField(max_length=50)
+    cancion = models.CharField(max_length=100)
+    localidad = models.CharField(max_length=50)
 
 
 class Meta:
